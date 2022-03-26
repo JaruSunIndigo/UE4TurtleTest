cbb82b8a6a33f3015f24b81e0a9e832bf61657ea
Sergey Koldaev <jkoldaev@gmail.com>
2022-03-26 23:13:31 +0300

    Fix compile time

--- Source/UE4TurtlesTest/UE4TurtlesTest.Build.cs
+++ Source/UE4TurtlesTest/UE4TurtlesTest.Build.cs
@@ -7,7 +7,8 @@ public class UE4TurtlesTest : ModuleRules
 	public UE4TurtlesTest(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
+		MinFilesUsingPrecompiledHeaderOverride = 1;
+		bFasterWithoutUnity = true;
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 	}
 }
